7b3ea60445305032811ba54ee4f109eaf036df65
AHDCorn <ahd68@cornell.edu>
2023-11-30 09:40:40 -0500

    Simple commentary updates on piston.py

--- src/carbon_manipulation/surfaces/piston.py
+++ src/carbon_manipulation/surfaces/piston.py
@@ -1,3 +1,6 @@
 # parameters: sheet dimensions, tube diameter
 # generate sheet, tube based on parameters. center will be the center of the tube
-# make hole in one sheet by taking out a circle. measure the circle (either a central atom or the center of a hexagon )
+# make hole in one sheet by taking out a circle. measure the circle (either a central atom or the center of a hexagon)
+
+# dimensions in terms of hexagons
+# 
