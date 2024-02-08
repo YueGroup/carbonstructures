d4b127528305033daac5c23622b6b7916e55c59f
AHDCorn <ahd68@cornell.edu>
2024-02-08 10:48:21 -0500

    20240208 10:48 AM
    Additions:
    - XYZ datafile format to generatesheet.py, generatesandwich.py
    
    Modifications:
    - Rounding coordinates to six decimal places for compatability with LAMMPS produced datafile formats
    
    Removals:
    - Unnecessary user specifications from graphene sheet class
    - Stray atom deletion from graphene sheet class

--- src/carbon_manipulation/surfaces/rectsheet.py
+++ src/carbon_manipulation/surfaces/rectsheet.py
@@ -6,15 +6,15 @@ class RectangularSheet(object):
     """
     Functions for initializing, generating coordinates for, and functionalizing rectangular graphene sheets
 
-    Notation notes: 
-        x-direction is zigzag side
-        y-direction is airmchair side
-        all lengths are in Angstroms
+    Defaults:
+        x-axis: zigzag side
+        y-axis: armchair side
+        z-axis: plane
     
     Instance attributes: 
         len1 [float]: total specified length in x-direction
         len2 [float]: total specified length in y-direction
-        CC_bond [float]: carbon-carbon bond length
+        CC [float]: carbon-carbon bond length
         hex_x [int]: number of hexagons in the x-direction
         hex_y [int]: number of hexagons in the y-direction
 
@@ -23,45 +23,35 @@ class RectangularSheet(object):
         provide the closest estimate that is smaller than the specified parameters
     """
 
-    def __init__(self, one, two, plane):
+    def __init__(self, x, y):
         """
         Creates a RectangularSheet instance:
             generated sheet is a VALID structure (no partial hexagons)
-            generated sheet has dimensional lengths LESS THAN OR EQUAL TO len1 and len2
+            generated sheet has dimensional lengths LESS THAN OR EQUAL TO x and y
 
         Preconditions: 
-            one, two are floats
-            plane is 0, 1, or 2
-                0: x = a, y = len1, z = len2
-                1: x = len1, y = a, z = len2
-                2: x = len1, y = len2, z = a
+            xlen, ylen are floats
         """
         # set CC bond length
-        self.CC_bond = 1.41
-        
-        # set plane (0: x = 0, 1: y = 0, 2: z = 0)
-        self.plane = plane
-        # check that the plane is valid
-        if plane not in [0, 1, 2]:
-            raise Exception("Not a valid plane for the surface!")
+        self.CC = 1.418
 
         # hexagon unit lengths
-        unit_1 = 2.0 * self.CC_bond * cos(pi / 6.0)
-        unit_2 = (1.0 + sin(pi / 6.0)) * self.CC_bond
+        xunit = 2.0 * self.CC * cos(pi / 6.0)
+        yunit = (1.0 + sin(pi / 6.0)) * self.CC
 
-        # calculate sheet dimensions in hexagonal units
-        self.hex_1 = (one - self.CC_bond * cos(pi / 6.0)) // unit_1
-        self.hex_2 = (two - self.CC_bond * sin(pi / 6.0)) // unit_2 
+        # calculate sheet dimensions in hexagonal units 
+        self.xhex = (x - self.CC * cos(pi / 6.0)) // xunit
+        self.yhex = (y - self.CC * sin(pi / 6.0)) // yunit
         
         # raise exception if specified parameters are too small
-        if (self.hex_1 <= 0) or (self.hex_2 <= 0): 
-            raise Exception("Dimensions too small!")
+        if (self.xhex <= 0) or (self.yhex <= 0): 
+            raise Exception("Sheet dimensions too small!")
         
         # length and width of sheet (no partial hexagons)
-        self.len1 = self.hex_1 * unit_1 + self.CC_bond * cos(pi / 6.0)
-        self.len2 = self.hex_2 * unit_2 + self.CC_bond * sin(pi / 6.0)
+        self.xlen = self.xhex * xunit + self.CC * cos(pi / 6.0)
+        self.ylen = self.yhex * yunit + self.CC * sin(pi / 6.0)
         
-    def generate_coords(self, one=0.0, two=0.0, three=0.0):
+    def generate_coords(self, z=0.0):
         """
         Returns an list of coordinates, in tuples (x,y), representing the rectangular graphene sheet
 
@@ -71,45 +61,47 @@ class RectangularSheet(object):
             z: z-coordinate of sheet (default 0.00)
         """
         # columns: number of unique x-coordinates
-        columns = 2 * self.hex_1 + 2
+        columns = 2 * self.xhex + 2
         # rows: number of unique y-coordinates
-        rows = 2 * self.hex_2 + 2
+        rows = 2 * self.yhex + 2
 
         # generate list of x-coordinates
-        coordinates1 = [one]
-        counter1 = 1
-        while counter1 < columns:
-            one += self.CC_bond * cos(pi / 6.0)
-            coordinates1.append(one)
-            counter1 += 1
+        xcoordlist = []
+        xcoord = -(self.CC * cos(pi / 6.0))
+        xcount = 0
+        while xcount < columns:
+            xcoordlist.append("{:.6f}".format(xcoord))
+            xcoord += self.CC * cos(pi / 6.0)
+            xcount += 1
 
         # generate list of y-coordinates
-        coordinates2 = [two]
-        counter2 = 1
-        while counter2 < rows:
-            if counter2 % 2: 
-                two += self.CC_bond * sin(pi / 6.0)
+        ycoordlist = []
+        ycoord = 0
+        ycount = 0
+        while ycount < rows:
+            ycoordlist.append("{:.6f}".format(ycoord))
+            if ycount % 2: 
+                ycoord += self.CC
             else:
-                two += self.CC_bond
-            coordinates2.append(two)
-            counter2 += 1
+                ycoord += self.CC * sin(pi / 6.0)
+            ycount += 1
 
         # generate coordinates
-        coordinates = [[coordinates1[ind1], coordinates2[ind2]]
-                for ind2 in range(len(coordinates2))
-                for ind1 in range(len(coordinates1))
+        coordinates = [[xcoordlist[ind1], ycoordlist[ind2]]
+                for ind2 in range(len(ycoordlist))
+                for ind1 in range(len(xcoordlist))
                 if (((ind2 + 1) % 4 == 0 or ind2 % 4 == 0) and ind1 % 2) or 
                    (not ((ind2 + 1) % 4 == 0 or ind2 % 4 == 0) and not ind1 % 2)]
 
-        if self.hex_2 % 2: 
-            coordinates.remove([coordinates1[-1], 0])
-            coordinates.remove([coordinates1[-1], coordinates2[-1]])
-        else: 
-            coordinates.remove([coordinates1[-1], 0])
-            coordinates.remove([0, coordinates2[-1]])
+#        if self.yhex % 2: 
+#            coordinates.remove([xcoordlist[-1], 0])
+#            coordinates.remove([xcoordlist[-1], ycoordlist[-1]])
+#        else: 
+#            coordinates.remove([xcoordlist[-1], 0])
+#            coordinates.remove([0, ycoordlist[-1]])
 
         for index in range(len(coordinates)):
-            coordinates[index].insert(self.plane, three)
+            coordinates[index].append("{:.6f}".format(z))
             coordinates[index] = tuple(coordinates[index])
         
         return coordinates
--- src/generatesheet.py
+++ src/generatesheet.py
@@ -7,36 +7,47 @@ if len(sys.argv[1:]) < 3:
     raise Exception("Not enough parameters specified!")
 
 # Generate graphene sheet object with size x,y in Angstroms
-structure = surfaces.RectangularSheet(float(sys.argv[1]),float(sys.argv[2]),int(sys.argv[3]))
+structure = surfaces.RectangularSheet(float(sys.argv[1]),float(sys.argv[2]))
 
 # Size of the system cell in Angstroms
-size1 = structure.len1
-size2 = structure.len2
+xsize = "{:.6f}".format(structure.xlen)
+ysize = "{:.6f}".format(structure.ylen)
 
 coordinates = structure.generate_coords()
 natoms = len(coordinates)
 
-# Write LAMMPS data file
-with open('rectsheet_' + str(size1) + "by" + str(size2) + '.data','w') as fdata:
-	# First line is a comment line 
-	fdata.write('Atoms for Graphene Sheet in LAMMPS\n\n')
+if sys.argv[3] == "lammps":
+	# Write LAMMPS data file
+	with open('rectsheet_' + str(xsize) + "by" + str(ysize) + '.data','w') as fdata:
+		# First line is a comment line 
+		fdata.write('Atoms for Graphene Sheet in LAMMPS\n\n')
 
-	#--- Header ---#
-	# Specify number of atoms and atom types 
-	fdata.write('{} atoms\n'.format(natoms))
-	fdata.write('{} atom type(s)\n'.format(1))
-	# Specify box dimensions
-	fdata.write('{} {} xlo xhi\n'.format(0.0, size1))
-	fdata.write('{} {} ylo yhi\n'.format(0.0, size2))
-	fdata.write('{} {} zlo zhi\n'.format(0.0, 0.0))
-	fdata.write('\n')
+		#--- Header ---#
+		# Specify number of atoms and atom types 
+		fdata.write('{} atoms\n'.format(natoms))
+		fdata.write('{} atom type(s)\n'.format(1))
+		# Specify box dimensions
+		fdata.write('{} {} xlo xhi\n'.format(0.000000, xsize))
+		fdata.write('{} {} ylo yhi\n'.format(0.000000, ysize))
+		fdata.write('{} {} zlo zhi\n'.format(0.000000, 0.000000))
+		fdata.write('\n')
 
-	# Atoms section
-	fdata.write('Atoms\n\n')
+		# Atoms section
+		fdata.write('Atoms\n\n')
 
-	# Write each position 
-	for i,pos in enumerate(coordinates):
-		fdata.write('{} 1 {} {} {}\n'.format(i+1,*pos))
-        
-		
-	# If you have bonds and angles, further sections below
+		# Write each position 
+		for i,pos in enumerate(coordinates):
+			fdata.write('{} 1 {} {} {}\n'.format(i+1,*pos))
+			
+		# If you have bonds and angles, further sections below
+
+if sys.argv[3] == "xyz":
+    # Write XYZ data file
+    with open('rectsheet_' + str(xsize) + "by" + str(ysize) + '.xyz','w') as fdata:
+        # Specify number of atoms
+        fdata.write('{}\n\n'.format(natoms))
+        for pos in coordinates:
+            fdata.write('C {} {} {}\n'.format(*pos))
+
+else:
+    raise Exception("File format not recognized!")
