cf8778ab78fcd0bfdd0b2ad5a6ac4c0ebe678336
itsnhiii <nhituyet6788@gmail.com>
2024-04-19 13:39:59 -0400

    add groups

---
+++ src/carbon_manipulation/functionalization/addgroups.py
@@ -0,0 +1,50 @@
+from math import sin, cos, radians, sqrt, asin,pi,floor
+import random
+import copy
+
+def random_gen(low,high,num):
+    index=[]
+    while len(index)<num+1:
+        n=random.randint(low,high)
+        if n not in index:
+            index.append(n)
+    return index
+
+def addgroup(coord,dens,pattern="random",type="OH"):
+    """
+    Returns a modified list of coordinates with functional groups added onto the structure
+
+    Parameters:
+    struct[obj]: an object of the carbon structure to functionalize
+    density[float]: R/C coverage density, with R being the functional group
+    pattern[str]: functionalized groups patterning: "random", "linear", "patch", or "patterned"
+    type[str]: chemical notation for functional group
+    """
+    mod_coord = copy.deepcopy(coord)
+    #choose C atom to add group (CAN REPLACE WITH GRAPH FUNCTIONS)
+    if pattern=="random":
+        C_coord = random_gen(0,len(coord)-1,floor(dens*len(coord)))
+    for c in C_coord:
+        xC = float(coord[c][0])
+        yC = float(coord[c][1])
+        zC = float(coord[c][2])
+    #retrieve bonds and angle data from functional group
+        if type=="OH":
+            O_coord=[]
+            CO=1.47
+            OH=0.98
+            COH=radians(107.9)
+            O_coord = ["{:.6f}".format(xC),"{:.6f}".format(yC),"{:.6f}".format(zC+CO)]
+
+            CH=sqrt(OH*OH+CO*CO-2*OH*CO*cos(COH))
+            ang= pi/2-asin(OH*sin(COH)/CH)
+            H_coord = ["{:.6f}".format(xC-cos(ang)*CH),"{:.6f}".format(yC),"{:.6f}".format(zC+sin(ang)*CH)]
+
+            mod_coord.append(O_coord)
+            mod_coord.append(H_coord)
+    return mod_coord
+
+
+
+# can have a library for the data of different functional groups
+# like bond angles,lengths => coordinates of atoms in different functional groups
---
+++ src/testadd.py
@@ -0,0 +1,87 @@
+from carbon_manipulation.functionalization import addgroups
+import carbon_manipulation.surfaces as surfaces
+import sys, getopt
+from math import cos, pi
+
+# Gather user input
+
+opts, args = getopt.getopt(sys.argv[1:],"hx:y:c:f:")
+
+x = ''
+y = ''
+c = 1.418
+f = ''
+
+for opt, arg in opts:
+    if opt == '-h':
+        print('python generatesheet.py -x <x-length> -y <y-length> (-c <cc bond length>) -f <file type>')
+        sys.exit()
+    elif opt == '-x':
+        x = arg
+    elif opt == '-y':
+        y = arg
+    elif opt == '-c':
+        cc = arg
+    elif opt == '-f':
+        f = arg
+
+# Exception for missing required inputs
+
+if x == '' or y == '' or f == '':
+    raise Exception("Missing required parameters!")
+
+# Generate graphene sheet object with size x, y and bond length c
+structure = surfaces.RectangularSheet(float(x),float(y),float(c))
+
+# Define box sizes
+xsize = "{:.6f}".format(structure.xlen)
+ysize = "{:.6f}".format(structure.ylen)
+xlo = "{:.6f}".format(-float(c) * cos(pi / 6.0))
+xhi = "{:.6f}".format(float(xsize) - float(xlo))
+ylo = "0.000000"
+yhi = "{:.6f}".format(float(ysize) + float(c))
+zlo = "0.000000"
+zhi = "0.000000"
+
+coordinates = structure.generate_coords()[0]
+natoms = len(coordinates)
+
+if f == "data":
+	# Write LAMMPS data file
+	with open('rectsheet_' + str(xsize) + "by" + str(ysize) + '.data','w') as fdata:
+		# First line is a comment line 
+		fdata.write('Atoms for Graphene Sheet in LAMMPS\n\n')
+
+		#--- Header ---#
+		# Specify number of atoms and atom types 
+		fdata.write('{} atoms\n'.format(natoms))
+		fdata.write('{} atom type(s)\n'.format(1))
+		# Specify box dimensions
+		fdata.write('{} {} xlo xhi\n'.format(xlo, xhi))
+		fdata.write('{} {} ylo yhi\n'.format(ylo, yhi))
+		fdata.write('{} {} zlo zhi\n'.format(zlo, zhi))
+		fdata.write('\n')
+
+		# Atoms section
+		fdata.write('Atoms\n\n')
+
+		# Write each position 
+		for i,pos in enumerate(coordinates):
+			fdata.write('{} 3 {} {} {}\n'.format(i+1,*pos))
+			
+		# If you have bonds and angles, further sections below
+
+elif f == "xyz":
+    # Write XYZ data file
+    with open('testsheet' + '.xyz','w') as fdata:
+        # Specify number of atoms
+        fdata.write('{}\n\n'.format(natoms))
+        new = addgroups.addgroup(coordinates,0.25)
+        for pos in new[:len(coordinates)]:
+            fdata.write('C {} {} {}\n'.format(*pos))
+
+        for pos in new[len(coordinates):]:
+            fdata.write('C {} {} {}\n'.format(*pos))
+
+else:
+    raise Exception("File format not recognized!")
