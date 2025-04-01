32d66f29176013acc7b71858eeeeb54ee3e424b8
itsnhiii <nhituyet6788@gmail.com>
2025-03-31 21:37:33 -0400

    write funct groups with their sheet in one block

--- src/carbonstructures/generate.py
+++ src/carbonstructures/generate.py
@@ -102,6 +102,13 @@ def main():
             zlo = "{:.6f}".format(bounds['min_z'])
             zhi = "{:.6f}".format(bounds['max_z'])
 
+            #print out wallid for lammps input file
+            print ("\n")
+            print("This is the atom id of each wall including the corresponding functional groups on it:")
+            print("(Note: Add water atoms to these ids to get the correct ids (i.e. 4500))")
+            print("Atoms id for wall1: " + "1:" + str(int(coordinates.number_of_nodes()/2)))
+            print("Atoms id for wall2: " + str(int(coordinates.number_of_nodes()/2+1)) + ":" + str(coordinates.number_of_nodes()))
+        
             with open(name + '.data','w') as fdata:
                 # First line is a comment line 
                 fdata.write('Atoms for Graphene Sheet in LAMMPS\n\n')
@@ -120,16 +127,35 @@ def main():
                 fdata.write('Atoms\n\n')
 
                 # Write each position 
-                for index,data in nodes:
-                    fdata.write('{} 1 {} 0 {} {} {}\n'.format(index+1,data['type'][1],*data['pos']))
+                # for index,data in nodes:
+                #     fdata.write('{} 1 {} 0 {} {} {}\n'.format(index+1,data['type'][1],*data['pos']))
+                # Sort by z-coordinate, then node index
+                sorted_nodes = sorted(
+                    coordinates.nodes(data=True),
+                    key=lambda item: (float(item[1]['pos'][2]), item[0])
+                )
+
+                # Write each position 
+                for new_index, (old_index, data) in enumerate(sorted_nodes, start=1):
+                    fdata.write('{} 1 {} 0 {} {} {}\n'.format(new_index, data['type'][1], *data['pos']))
+                
         
         # XYZ datafile format (for quick VMD visualization)
         elif format == '2':
             with open(name + '.xyz','w') as fdata:
                 fdata.write('{}\n\n'.format(coordinates.number_of_nodes()))
-                for index,data in nodes:
-                    fdata.write('{} {} {} {}\n'.format(data['type'][0],*data['pos']))
-    
+                # for index,data in nodes:
+                #     fdata.write('{} {} {} {}\n'.format(data['type'][0],*data['pos']))
+
+                # Sort by z-coordinate, then by node index to preserve order
+                sorted_nodes = sorted(
+                    coordinates.nodes(data=True),
+                    key=lambda item: (float(item[1]['pos'][2]), item[0])
+                )
+
+                for _, data in sorted_nodes:
+                    fdata.write('{} {} {} {}\n'.format(data['type'][0], *data['pos']))
+
     # Exits the script any time user attempts a keyboard interrupt (Ctrl + C)
     except KeyboardInterrupt:
         print("\nProgram interrupted! Exiting gracefully...")
